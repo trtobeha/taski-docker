30650bdd63bc5316c8fc47d5196040ca0d145f53
trtobeha <trtobeha@gmail.com>
2023-08-14 13:06:58 +0300

    nc

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,10 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    'taskihw-yp.ddns.net',
+]
 
 
 # Application definition
